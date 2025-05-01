aeab07bd35e4b14286d89cf261d8088c83809a9c
thiagozalinsky <121072112+thiagozalinsky@users.noreply.github.com>
2025-05-01 16:03:34 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: ["./index.html", "./src/**/*.{js,jsx}"],
-    theme: {
-      extend: {},
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        mercadolivre: {
+          yellow: '#ffe600',
+          blue: '#3483fa',
+          gray: '#f5f5f5',
+          text: '#333333',
+        },
+      },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
