f317b94ebae4acdb1ad240ea0d096677b7116536
ThiagoZalinsky <seu-email-da-conta-thiagoleonardodasilva.com>
2025-05-01 16:21:06 -0300

    Build do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-=======
-// tailwind.config.js
->>>>>>> aeab07bd35e4b14286d89cf261d8088c83809a9c
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        mercadolivre: {
-          yellow: '#ffe600',
-          blue: '#3483fa',
-          gray: '#f5f5f5',
-          text: '#333333',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
